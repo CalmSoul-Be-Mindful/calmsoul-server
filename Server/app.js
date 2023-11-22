b511e5dca70a8573476340ff22a0eb7069f8150a
Adjip25 <adjipratama18@gmail.com>
2023-11-22 17:25:51 +0700

    failed create video chat & created api doc

--- Server/app.js
+++ Server/app.js
@@ -4,18 +4,46 @@ if (process.env.NODE_ENV !== "production") {
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const router = require("./routers");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const http = require("http");
 const app = express();
 // const port = 3000;
+const server = http.createServer(app);
+// // const socket = require("socket.io");
+// const io = socket(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
 
 app.use(errorHandler);
 
