d90befbde60533b7a33f341f735abe029af6f467
0wenB <bryanowen.code@gmail.com>
2023-11-22 15:20:03 +0700

    feat: done live video

--- Server/app.js
+++ Server/app.js
@@ -33,7 +33,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    console.log("triggered");
+    // console.log(userToCall, signalData, from, name);
     io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
